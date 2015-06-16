421b84d76751db599eba936947c5b0aeee52f6aa
Kyle L. Jensen <kljensen@gmail.com>
2015-06-16 12:26:17 -0400

    Add some logging to track down issue

--- both/games/ultimatum/collections/ultimatum.js
+++ both/games/ultimatum/collections/ultimatum.js
@@ -122,6 +122,7 @@ var payoffCDF = function(player1amounts, player2amounts){
       // If the current amount is equal to the max
       // offer, add those to the running sum.
       while(j >= 0 && i === sortedPlayer1amounts[j]){
+        console.log('adding ', i, 'to running sum');
         runningSum += i;
         j--;
       }
