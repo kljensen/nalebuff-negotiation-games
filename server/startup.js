97432c2119f48a5a9fc47d6dd7ac1a4d617a53b4
Kyle L. Jensen <kljensen@gmail.com>
2015-06-24 09:19:04 -0400

    Ensure users cannot update their profile

--- server/startup.js
+++ server/startup.js
@@ -8,4 +8,10 @@ Meteor.startup(function(){
 	    clientId: Meteor.settings.coursera.clientId,
 	    secret: Meteor.settings.coursera.secret
 	 });
+
+    Meteor.users.deny({
+      update: function() {
+        return true;
+      }
+    });
 })
