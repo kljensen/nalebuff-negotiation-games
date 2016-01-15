bff86d3e5927ea6e5906bace13a9f04b2003935f
Kyle L. Jensen <kljensen@gmail.com>
2016-01-15 15:04:23 -0500

    Add support for Yale CAS

--- server/startup.js
+++ server/startup.js
@@ -1,17 +1,21 @@
 Meteor.startup(function(){
-	ServiceConfiguration.configurations.remove({
-	      service: 'coursera'
-	});
-	 
-	 ServiceConfiguration.configurations.insert({
-	    service: 'coursera',
-	    clientId: Meteor.settings.coursera.clientId,
-	    secret: Meteor.settings.coursera.secret
-	 });
 
-    Meteor.users.deny({
-      update: function() {
-        return true;
-      }
-    });
+	Meteor.users.deny({
+		update: function() {
+			return true;
+		}
+	});
+
+	if (Meteor.settings.public.auth !== 'cas') {
+		ServiceConfiguration.configurations.remove({
+			service: 'coursera'
+		});
+
+		ServiceConfiguration.configurations.insert({
+			service: 'coursera',
+			clientId: Meteor.settings.coursera.clientId,
+			secret: Meteor.settings.coursera.secret
+		});
+
+	}
 })
