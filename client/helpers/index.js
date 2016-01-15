c2598be5dcb105211b097f3b8b816036b5b613c1
Kyle L. Jensen <kljensen@gmail.com>
2016-01-15 15:12:00 -0500

    Add missing file

---
+++ client/helpers/index.js
@@ -0,0 +1,22 @@
+Template.index.helpers({
+  siteName: function(){
+    return Meteor.settings.public.siteName;
+  },
+  usingCas: function(){
+    if(Meteor.settings.public.auth === 'cas'){
+      return true;
+    }
+    return false;
+  }
+});
+
+Template.casLoginButton.events({
+  'click button': function () {
+    event.preventDefault();
+
+    Meteor.loginWithCas(function(){
+      console.log('Woot Meteor.user()=', Meteor.user());
+    });
+    return false;
+  }
+});
